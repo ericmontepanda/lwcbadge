107f91295bb0b774f63803aad95745e0bd1c8e8b
eric montelibano <eric.montelibano@pwc.com>
2020-06-27 16:07:19 -0400

    complete boat tile

---
+++ force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
@@ -0,0 +1,55 @@
+import {
+    LightningElement,
+    track,
+    wire
+} from 'lwc';
+
+import getBoatTypes from '@salesforce/apex/BoatDataService.getBoatTypes';
+
+export default class BoatSearchForm extends LightningElement {
+    selectedBoatTypeId = '';
+
+    // Private
+    error = undefined;
+
+    // Needs explicit track due to nested data
+    @track searchOptions;
+
+    // Wire a custom Apex method
+    @wire (getBoatTypes) BoatTypes({
+        error,
+        data
+    }) {
+        if (data) {
+            console.log('...' + JSON.stringify(data));
+            this.searchOptions = data.map(type => {
+                // TODO: complete the logic
+                return {
+                    label: type.Name,
+                    value: type.Id
+                }
+            });
+            this.searchOptions.unshift({
+                label: 'All Types',
+                value: ''
+            });
+        } else if (error) {
+            this.searchOptions = undefined;
+            this.error = error;
+        }
+    }
+
+    // Fires event that the search option has changed.
+    // passes boatTypeId (value of this.selectedBoatTypeId) in the detail
+    handleSearchOptionChange(event) {
+        // Create the const searchEvent
+        // searchEvent must be the new custom event search
+        this.selectedBoatTypeId = event.detail.value;
+        console.log('EVENT>>> ' +JSON.stringify(event.detail.value));
+        const searchEvent = new CustomEvent('search', {
+            boatTypeId: this.selectedBoatTypeId
+        });
+        console.log('EVENT>>> ' +JSON.stringify(searchEvent));
+        this.dispatchEvent(searchEvent);
+    }
+}
---
+++ force-app/main/default/lwc/boatTile/boatTile.js
@@ -0,0 +1,39 @@
+import {
+    LightningElement,
+    api,
+    track
+} from 'lwc';
+
+export default class BoatTile extends LightningElement {
+    @api boat;
+    @api selectedBoatId;
+    @track boatId;
+
+    // Getter for dynamically setting the background image for the picture
+    get backgroundStyle() {
+        return `background-image:url(${this.boat.Picture__c})`;
+        //return `background-image:url(${this.boat.Picture__c;})`;
+    }
+
+    // Getter for dynamically setting the tile class based on whether the
+    // current boat is selected
+    get tileClass() {
+        const TILE_WRAPPER_SELECTED_CLASS = 'tile-wrapper selected';
+        const TILE_WRAPPER_UNSELECTED_CLASS = 'tile-wrapper';
+        if (this.selectedBoatId === this.boat.Id){
+            return TILE_WRAPPER_SELECTED_CLASS;
+        } else {
+            return TILE_WRAPPER_UNSELECTED_CLASS;
+        }
+    }
+
+    // Fires event with the Id of the boat that has been selected.
+    selectBoat() {
+
+        const selectedEvent = new CustomEvent('boatselect', {
+            detail: this.boat.Id
+        });
+
+        this.dispatchEvent(selectedEvent);
+    }
+}
